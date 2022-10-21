cc0cd638b14482e15d00b9a44fc87528999f833f
shakibul <shakibul.alam@bracits.com>
2022-10-21 23:54:27 +0600

    if, else, combine two condition with or

--- hello.go
+++ hello.go
@@ -7,8 +7,18 @@ func main() {
     // x := 1.0
 	// y := 2.0
 
-	x, y := 1.0, 2.0
-	z := 3 // unused variable will occure compilation error. 
+	x, y := 1.0, 5.0
+	//z := 3 // unused variable will occure compilation error. 
+
+	if x > 4 {
+		fmt.Println("x is greater than 4")
+	}else {
+		fmt.Println("x is less or equal to 4")
+	}
+
+	if y == 5 || y > 5 {
+		fmt.Println("y is equal to 5 or greater than 5")
+	}
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
