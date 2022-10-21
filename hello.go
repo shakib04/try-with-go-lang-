f2aabebe52be2e568313a8c4f422a315c9f82546
shakibul <shakibul.alam@bracits.com>
2022-10-21 23:13:08 +0600

    direct x and y initialize

--- hello.go
+++ hello.go
@@ -4,10 +4,8 @@ import "fmt"
 //import "rsc.io/quote"
 
 func main() {
-	var x float64
-	var y float64
-    x = 1
-	y = 2
+    x := 1.0
+	y := 2.0
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
