65e2cef8a72c5f62d4e13e2f2b67569c9c0d1efc
shakibul <shakibul.alam@bracits.com>
2022-10-21 23:26:04 +0600

    unused variable will occure compilation error.

--- hello.go
+++ hello.go
@@ -8,6 +8,7 @@ func main() {
 	// y := 2.0
 
 	x, y := 1.0, 2.0
+	z := 3 // unused variable will occure compilation error. 
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
