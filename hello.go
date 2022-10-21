0a0ea78102c1b550ceb4b1c6d4a245c0f09f871c
shakibul <shakibul.alam@bracits.com>
2022-10-21 23:16:35 +0600

    initialize in a single line

--- hello.go
+++ hello.go
@@ -4,8 +4,11 @@ import "fmt"
 //import "rsc.io/quote"
 
 func main() {
-    x := 1.0
-	y := 2.0
+    // x := 1.0
+	// y := 2.0
+
+	x, y := 1.0, 2.0
+
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
